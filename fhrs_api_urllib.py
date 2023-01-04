b4550461eaacfef9e3bd26c7699d7a74fea27f74
minster-hatter <52294953+minster-hatter@users.noreply.github.com>
2023-01-04 13:09:07 +0000

    add urllib example for FHRS API

---
+++ fhrs_api_urllib.py
@@ -0,0 +1,33 @@
+import urllib.request
+import json
+
+# Parameters for the API call.
+lang = "en-GB"
+sort_by = "Relevance"
+sector = "7845"
+page = "1"
+page_size = "5"
+data_type = "json"
+
+url = (
+    f"https://api1-ratings.food.gov.uk/enhanced-search/"
+    f"{lang}/%5E/%5E/{sort_by}/{sector}/%5E/%5E/0/{page}/"
+    f"{page_size}/{data_type}"
+)
+
+# Fetch the data using the API.
+with urllib.request.urlopen(url) as resp:
+    raw_data = resp.read()
+    j_data = json.loads(raw_data)
+
+# Extract the establishments data from the raw data.
+estab_data = j_data["FHRSEstablishment"]["EstablishmentCollection"][
+    "EstablishmentDetail"
+]
+
+# Filter the data to only the first locality.
+local_data = [
+    estab
+    for estab in estab_data
+    if estab["LocalAuthorityName"] == estab_data[0]["LocalAuthorityName"]
+]
